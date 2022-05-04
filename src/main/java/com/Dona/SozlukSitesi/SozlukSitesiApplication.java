9eb8ad4d5e1cc774787bcaf9c59f68010c0139b4
Metehan Gültekin <mgmetehan@hotmail.com>
2022-05-04 14:25:09 +0300

    first commit

---
+++ src/main/java/com/Dona/SozlukSitesi/SozlukSitesiApplication.java
@@ -0,0 +1,13 @@
+package com.Dona.SozlukSitesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SozlukSitesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SozlukSitesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dona/SozlukSitesi/SozlukSitesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dona.SozlukSitesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SozlukSitesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
